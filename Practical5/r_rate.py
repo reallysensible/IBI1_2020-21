e7a469eb0d0b5145c4d1e622e6485ef70b95182d
Jiayi Fan <3200112417@zju.edu.cn>
2021-05-18 17:53:22 +0800

    change the pseudocode and delete an unnecessary variable

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,18 +1,15 @@
 n = 84
 r = 1.1
+
+# Repeat for five times:
+#   newly infected people = r * total infected people in last round
+#   total infected people this round = newly infected people + total infected people in last round
+#   done
 for i in range (1,6):
-    new = r * n
-    total = new + n
-    n = total
+    new = r * n # total infected people in last round
+    n = new + n # now n becomes the total infected people this round
 
+# approximate the result (I ignore the decimals as numbers of people only can be integers.)
 import math
-total = math.floor(total)
-print('The total number of individuals infected after 5 generations is', total, 'if the r rate is', r)
-
-
-# Check for r = 1.1 (should be 3430 infections)
-
-# new is newly infected people
-# b is infected people in last round
-# c is the total ifected people after this round
-# five rounds of infection
+n = math.floor(n)
+print('The total number of individuals infected after 5 generations is', n, 'if the r rate is', r)
